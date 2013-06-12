bba4c85b008c583ec3687f43118eb310f516aa64
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2013-06-12 10:42:56 +0200

    additional clarifications

--- AR.Drone.Client/Commands/FlightMode.cs
+++ AR.Drone.Client/Commands/FlightMode.cs
@@ -5,7 +5,13 @@ namespace AR.Drone.Client.Commands
     [Flags]
     public enum FlightMode
     {
+        /// <summary>
+        /// The hover.
+        /// </summary>
         Hover = 0,
+        /// <summary>
+        /// The progressive is an enabling bit.
+        /// </summary>
         Progressive = 1 << 0,
         /// <summary>
         /// The combined yaw. Usage: FlightMode.Progressive | FlightMode.CombinedYaw
