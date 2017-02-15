ac4e588504aa2b5235b6bf0911ea1bb40ac4aada
Ruslan Balanukhin <ruslan.balanukhin@gmail.com>
2017-02-15 20:10:02 +0100

    remove bogus header

--- AR.Drone.Video/VideoDecoder.cs
+++ AR.Drone.Video/VideoDecoder.cs
@@ -1,28 +1,3 @@
-﻿// =============================================================================
-// =
-// =   FILE:			VideoDecoder.cs
-// =
-// =============================================================================
-// =                                                                        
-// = COPYRIGHT: Title to, ownership of and copyright of this software is
-// = vested in Copyright 2003-2014  Baker Hughes Reservoir Software BV.
-// = is a wholly-owned subsidiary of Baker Hughes Incorporated.
-// = All rights reserved.
-// =
-// = Neither the whole nor any part of this software may be
-// = reproduced, copied, stored in any retrieval system or
-// = transmitted in any form or by any means without prior
-// = written consent of the copyright owner.
-// =
-// = This software and the information and data it contains is
-// = confidential. Neither the whole nor any part of the
-// = software and the data and information it contains may be
-// = disclosed to any third party without the prior written
-// = consent of Copyright 2003-2014 Baker Hughes Reservoir Software BV, a
-// = wholly-owned subsidiary of Baker Hughes Incorporated 
-// =                                                                          
-// =============================================================================
-
 using System.Diagnostics;
 using AR.Drone.Infrastructure;
 using AR.Drone.Video.Exceptions;
